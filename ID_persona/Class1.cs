073a9ebbe2fa6679d23c516bcb52247123649c78
Aleguid97 <aleguid97@gmail.com>
2024-01-30 15:33:56 +0100

    Aggiungere i file di progetto.

---
+++ ID_persona/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID_persona
+{
+    public class ID_persona
+    {
+        public string _nome { get; set; }
+        public string _cognome { get; set; }
+        public int _età { get; set; }
+        public string _CF { get; set; }
+        public string _indirizzo { get; set; }
+
+       public ID_persona(string nome, string cognome, int età, string CF, string indirizzo)
+        {
+            _nome = nome;
+            _cognome = cognome;
+            _età = età;
+            _CF = CF;
+            _indirizzo = indirizzo;
+
+        }
+        public void nome()
+        {
+            _nome = "Alessio";
+        }
+
+        public void cognome() 
+        {
+            _cognome = "Guida";
+        }
+
+        public void età ()
+        {
+            _età = 27;
+        }
+
+        public void CF ()
+        {
+            _CF = "GDULSS97A10M088Q";
+        }
+
+        public void indirizzo ()
+        {
+            _indirizzo = "Via Neghelli 91";
+        }
+
+        public void getDettagli()
+        {
+            Console.WriteLine($"il tuo nome è {_nome} {_cognome} hai {27} anni abiti in {_indirizzo}. Il tuo codice fiscale è {_CF}");
+        }
+    }
+}
---
+++ ID_persona/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID_persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         ID_persona tizio = new ID_persona("","",0,"","");
+            tizio.nome();
+            tizio.cognome();
+            tizio.indirizzo();
+            tizio.età();
+            tizio.CF();
+
+            tizio.getDettagli();
+        }
+    }
+}
